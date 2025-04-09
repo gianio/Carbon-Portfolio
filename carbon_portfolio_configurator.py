b7d0afc3fc7339754e8f445145a61f6f2ce24a37
gianio <34304264+gianio@users.noreply.github.com>
2025-04-09 09:47:47 +0200

    Update carbon_portfolio_configurator.py

--- carbon_portfolio_configurator.py
+++ carbon_portfolio_configurator.py
@@ -10,10 +10,10 @@ import seaborn as sns  # For potentially more appealing styles
 st.markdown("""
     <style>
     body {
-        background-color: #9ec143;
+        background-color: #D9E1C8;
     }
     .stApp {
-        background-color: #9ec143;
+        background-color: #D9E1C8;
     }
     </style>
 """, unsafe_allow_html=True)
@@ -204,9 +204,7 @@ if df:
             yaxis2_title='Average Price (€)',
             legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
             barmode='stack',
-            template="plotly_white", # Use a clean template
-            plot_bgcolor='#9ec143',  # Set the plot background color
-            paper_bgcolor='#9ec143'     # Set the paper (surrounding) background color
+            template="plotly_white" # Use a clean template
         )
         
         st.plotly_chart(fig)
